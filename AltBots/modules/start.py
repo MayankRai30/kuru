369b1cb3593a1126b9ebc1a844f7cd83c5bee1fb
MayankRai30 <102607416+MayankRai30@users.noreply.github.com>
2023-09-18 17:01:57 +0530

    Add files via upload

---
+++ AltBots/modules/start.py
@@ -0,0 +1,40 @@
+from telethon import __version__, events, Button
+
+from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
+
+
+START_BUTTON = [
+    [
+        Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
+    ],
+    
+]
+
+
+@X1.on(events.NewMessage(pattern="/start"))
+@X2.on(events.NewMessage(pattern="/start"))
+@X3.on(events.NewMessage(pattern="/start"))
+@X4.on(events.NewMessage(pattern="/start"))
+@X5.on(events.NewMessage(pattern="/start"))
+@X6.on(events.NewMessage(pattern="/start"))
+@X7.on(events.NewMessage(pattern="/start"))
+@X7.on(events.NewMessage(pattern="/start"))
+@X8.on(events.NewMessage(pattern="/start"))
+@X9.on(events.NewMessage(pattern="/start"))
+@X10.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+        AltBot = await event.client.get_me()
+        bot_name = AltBot.first_name
+        bot_id = AltBot.id
+        TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [MAYANK](https://t.me/Mayankrai3008)**\n\n"
+        TEXT += f"» **xʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
+        TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
+        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
+        await event.client.send_file(
+                    event.chat_id,
+                    "http://telegra.ph/file/70a66ba8bd06ad6b5f00d.jpg",
+                    caption=TEXT, 
+                    buttons=START_BUTTON
+                )
